dc8b1d6ea17ddf8673d23e6d887be1d73407e48a
unclechuen <johnny@johnnyip.com>
2023-03-16 05:12:39 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use the latest Node.js LTS version as the base image
+FROM node:lts-alpine
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Copy the package.json and package-lock.json files to the container
+COPY package*.json ./
+
+# Install dependencies using npm
+RUN npm install --production
+
+# Copy the rest of the application files to the container
+COPY . .
+
+# Set the environment variables for the Telegram bot API token
+ENV TELEGRAM_BOT_TOKEN=<your-telegram-bot-api-token>
+
+# Start the application using npm
+CMD ["npm", "start"]
---
+++ index.js
@@ -0,0 +1,83 @@
+// const fs = require('fs');
+const TelegramBot = require('node-telegram-bot-api');
+
+// Replace YOUR_TOKEN_HERE with your actual bot token
+const bot = new TelegramBot('YOUR_TOKEN_HERE', { polling: true });
+
+
+// Handle /start command
+bot.onText(/\/start/, (msg) => {
+    // bot.sendMessage(msg.chat.id, 'Click to get the TG sticker download link\n  [Link](google.com)', options);
+
+    bot.sendMessage(msg.chat.id, `
+[Telegram Machiko1](https://t.me/addstickers/Machiko_1_by_johnnyip_stickerbot)
+
+[Telegram Machiko2](https://t.me/addstickers/Machiko_2_by_johnnyip_stickerbot)
+
+[Telegram Machiko3](https://t.me/addstickers/Machiko_3_by_johnnyip_stickerbot)
+
+[Telegram Machiko4](https://t.me/addstickers/Machiko_4_by_johnnyip_stickerbot)
+
+[Telegram Machiko5](https://t.me/addstickers/Machiko_5_by_johnnyip_stickerbot)
+
+[Telegram Machiko6](https://t.me/addstickers/Machiko_6_by_johnnyip_stickerbot)
+
+[Telegram Machiko7](https://t.me/addstickers/Machiko_7_by_johnnyip_stickerbot)
+
+[Telegram Machiko8](https://t.me/addstickers/Machiko_8_by_johnnyip_stickerbot)
+
+[Telegram Machiko9](https://t.me/addstickers/Machiko_9_by_johnnyip_stickerbot)
+
+[Telegram Machiko10](https://t.me/addstickers/Machiko_10_by_johnnyip_stickerbot)
+
+
+
+[Telegram Machiko11](https://t.me/addstickers/Machiko_11_by_johnnyip_stickerbot)
+
+[Telegram Machiko12](https://t.me/addstickers/Machiko_12_by_johnnyip_stickerbot)
+
+[Telegram Machiko13](https://t.me/addstickers/Machiko_13_by_johnnyip_stickerbot)
+
+[Telegram Machiko14](https://t.me/addstickers/Machiko_14_by_johnnyip_stickerbot)
+
+[Telegram Machiko15](https://t.me/addstickers/Machiko_15_by_johnnyip_stickerbot)
+
+[Telegram Machiko16](https://t.me/addstickers/Machiko_16_by_johnnyip_stickerbot)
+
+[Telegram Machiko17](https://t.me/addstickers/Machiko_17_by_johnnyip_stickerbot)
+
+[Telegram Machiko18](https://t.me/addstickers/Machiko_18_by_johnnyip_stickerbot)
+
+[Telegram Machiko19](https://t.me/addstickers/Machiko_19_by_johnnyip_stickerbot)
+
+[Telegram Machiko20](https://t.me/addstickers/Machiko_20_by_johnnyip_stickerbot)
+
+
+[Telegram Machiko21](https://t.me/addstickers/Machiko_21_by_johnnyip_stickerbot)
+
+[Telegram Machiko22](https://t.me/addstickers/Machiko_22_by_johnnyip_stickerbot)
+
+[Telegram Machiko23](https://t.me/addstickers/Machiko_23_by_johnnyip_stickerbot)
+
+[Telegram Machiko24](https://t.me/addstickers/Machiko_24_by_johnnyip_stickerbot)
+
+[Telegram Machiko25](https://t.me/addstickers/Machiko_25_by_johnnyip_stickerbot)
+
+[Telegram Machiko26](https://t.me/addstickers/Machiko_26_by_johnnyip_stickerbot)
+
+[Telegram Machiko27](https://t.me/addstickers/Machiko_27_by_johnnyip_stickerbot)
+
+[Telegram Machiko28](https://t.me/addstickers/Machiko_28_by_johnnyip_stickerbot)
+
+[Telegram Machiko29](https://t.me/addstickers/Machiko_29_by_johnnyip_stickerbot)
+
+[Telegram Machiko30](https://t.me/addstickers/Machiko_30_by_johnnyip_stickerbot)
+
+請慢用～
+
+`, { parse_mode: 'MarkdownV2' });
+
+});
+
+
+
